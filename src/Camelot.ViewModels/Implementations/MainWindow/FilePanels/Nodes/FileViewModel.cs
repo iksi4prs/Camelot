9d49672ccab51367ed564be421d57ea7110a8f36
iksi4prs <iksi4prs@gmail.com>
2023-09-11 11:39:49 +0300

    added fallback to builtin icons, when there is no shell icon

--- src/Camelot.ViewModels/Implementations/MainWindow/FilePanels/Nodes/FileViewModel.cs
+++ src/Camelot.ViewModels/Implementations/MainWindow/FilePanels/Nodes/FileViewModel.cs
@@ -18,6 +18,7 @@ using PixelSize = Avalonia.PixelSize;
 using Vector = Avalonia.Vector;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System;
 
 namespace Camelot.ViewModels.Implementations.MainWindow.FilePanels.Nodes;
 
@@ -28,7 +29,11 @@ public class FileViewModel : FileSystemNodeViewModelBase, IFileViewModel
     private readonly ISystemIconsService _systemIconsService;
     private readonly IShellLinksService _shellLinksService;
     private long _size;
-
+    private Bitmap _systemIcon = null;
+    // Helper to load icon only on demand.
+    // Can't use icon member itself, since null is valid value,
+    // in case file has no shell icon.
+    private bool _loadedShellIcon = false;
     public string Extension { get; set; }
 
     public long Size
@@ -66,95 +71,99 @@ public class FileViewModel : FileSystemNodeViewModelBase, IFileViewModel
         _shellLinksService = shellLinksService;
     }
 
-    
-    private Bitmap _systemIcon = null;
+
+    private Bitmap GetShellIcon()
+    {
+        Bitmap result = null;
+        string path = FullPath;
+        var isLink = _shellLinksService.IsShellLink(FullPath);
+        if (isLink)
+        {
+            var resolved = _shellLinksService.ResolveLink(FullPath);
+            // Check if resolved still exists, sometimes the target of .lnk files
+            // dont exist anymore, or links to a folder
+            if (!File.Exists(resolved))
+            {
+                if (Directory.Exists(resolved))
+                {
+                    int dbg = 9;
+                    dbg = 8;
+                }
+                // WIP333 - return some SVG
+                return null;
+            }
+            // cotinue with new path
+            path = resolved;
+        }
+
+        /*              {
+      var resolved = ShellLink.ResolveLink(path);
+      // need again to check if resolved is extension or path,
+      // or maybe even a folder.
+      var type = GetIconType(resolved);
+      if (type == ISystemIconsService.SystemIconType.Extension)
+      {
+          var ext2 = Path.GetExtension(resolved);
+          return GetIconForExtension(ext2);
+      }
+      else
+      {
+          // Continue with flow for path.
+          // Check if resolved still exists, sometimes the target of .lnk files
+          // dont exist anymore
+          if (!File.Exists(resolved))
+          {
+              // WIP333 - return some SVG
+              return null;
+          }
+          path = resolved;
+      }
+  }
+          }
+  */
+        // TODO WIP333
+        // first - check if in cache
+        // TODO WIP333
+        // depending on implemntaion of Limux,
+        // maybe move cache to common layer, eg
+        // SystemIconsCache (impelmeted under the "Camelot.Services.AllPlatforms" ?)
+
+        // not in cache, get from system/shell
+        System.Drawing.Image image;
+        var t = _systemIconsService.GetIconType(path);
+        switch (t)
+        {
+            case ISystemIconsService.SystemIconType.Extension:
+                var ext = System.IO.Path.GetExtension(path);
+                if (string.IsNullOrEmpty(ext))
+                {
+                    // a file with no extension.
+                    // WIP333 - return some default SVG
+                    return null;
+                }
+                else
+                {
+                    image = _systemIconsService.GetIconForExtension(ext);
+                }
+                break;
+            case ISystemIconsService.SystemIconType.Path:
+                image = _systemIconsService.GetIconForPath(path);
+                break;
+            default:
+                throw new System.Exception();
+        }
+        result = ConvertToAvaloniaBitmap(image);
+        return result;
+
+    }
     public Bitmap SystemIcon
     {
         get
         {
-            if (_systemIcon == null)
+            if (!_loadedShellIcon)
             {
-                string path = FullPath;
-                var isLink = _shellLinksService.IsShellLink(FullPath);
-                if (isLink)
-                {
-                    var resolved = _shellLinksService.ResolveLink(FullPath);
-                    // Check if resolved still exists, sometimes the target of .lnk files
-                    // dont exist anymore, or links to a folder
-                    if (!File.Exists(resolved))
-                    {
-                        if (Directory.Exists(resolved))
-                        {
-                            int dbg = 9;
-                            dbg = 8;
-                        }
-                        // WIP333 - return some SVG
-                        return null;
-                    }
-                    // cotinue with new path
-                    path = resolved;
-                }
-        
-              /*              {
-            var resolved = ShellLink.ResolveLink(path);
-            // need again to check if resolved is extension or path,
-            // or maybe even a folder.
-            var type = GetIconType(resolved);
-            if (type == ISystemIconsService.SystemIconType.Extension)
-            {
-                var ext2 = Path.GetExtension(resolved);
-                return GetIconForExtension(ext2);
-            }
-            else
-            {
-                // Continue with flow for path.
-                // Check if resolved still exists, sometimes the target of .lnk files
-                // dont exist anymore
-                if (!File.Exists(resolved))
-                {
-                    // WIP333 - return some SVG
-                    return null;
-                }
-                path = resolved;
-            }
-        }
-                }
-        */
-                // TODO WIP333
-                // first - check if in cache
-                // TODO WIP333
-                // depending on implemntaion of Limux,
-                // maybe move cache to common layer, eg
-                // SystemIconsCache (impelmeted under the "Camelot.Services.AllPlatforms" ?)
-
-                // not in cache, get from system/shell
-                System.Drawing.Image image;
-
-
-
-               var t = _systemIconsService.GetIconType(path);
-               switch(t)
-               {
-                   case ISystemIconsService.SystemIconType.Extension:
-                       var ext = System.IO.Path.GetExtension(path);
-                        if (string.IsNullOrEmpty(ext))
-                        {
-                            // a file with no extension.
-                            // WIP333 - return some default SVG
-                            return null;
-                        }
-                        else
-                        {
-                            image = _systemIconsService.GetIconForExtension(ext);
-                        }
-                       break;
-                    case ISystemIconsService.SystemIconType.Path:
-                       image = _systemIconsService.GetIconForPath(path);
-                       break;
-                   default:
-                       throw new System.Exception();
-               }
-               _systemIcon = ConvertToAvaloniaBitmap(image);
+                _systemIcon = GetShellIcon();
+                _loadedShellIcon = true;
             }
             return _systemIcon;
         }
@@ -167,10 +176,27 @@ public class FileViewModel : FileSystemNodeViewModelBase, IFileViewModel
             // TODO WIP333
             // 1) check if window
             // 2) check value from settings
-            return true;
+
+            // if not first time, and already have value
+            if (!_loadedShellIcon)
+            {
+                _systemIcon = GetShellIcon();
+                _loadedShellIcon = true;
+            }
+
+            if (_systemIcon != null)
+            {
+                return true;
+            }
+            else
+            {
+                // file has no shell icon, so fallback to use builtin icons
+                return false;
+            }
         }
     }
 
+    
     // TODO WIP333 - move to extnetsion method or helper file
     // see https://github.com/AvaloniaUI/Avalonia/discussions/5908
     public static Bitmap ConvertToAvaloniaBitmap(System.Drawing.Image bitmap)
