ac0c995520d5d99cb03a96d93435dcc82cf4447d
iksi4prs <iksi4prs@gmail.com>
2023-09-20 23:29:21 +0300

    moved strings to resources

--- src/Camelot/Properties/Resources.Designer.cs
+++ src/Camelot/Properties/Resources.Designer.cs
@@ -314,7 +314,7 @@ namespace Camelot.Properties {
                 return ResourceManager.GetString("HelpHeader", resourceCulture);
             }
         }
-        
+
         public static string LanguageWithColon {
             get {
                 return ResourceManager.GetString("LanguageWithColon", resourceCulture);
@@ -888,6 +888,30 @@ namespace Camelot.Properties {
         public static string GoToParentDirectory {
             get {
                 return ResourceManager.GetString("GoToParentDirectory", resourceCulture);
+            }
+        }
+        
+	public static string IconsWithColon {
+            get {
+                return ResourceManager.GetString("IconsWithColon", resourceCulture);
+            }
+        }
+	
+	public static string FilesIconsWithColon {
+            get {
+                return ResourceManager.GetString("FilesIconsWithColon", resourceCulture);
+            }
+        }
+        
+	public static string ChangesRequireRestart {
+            get {
+                return ResourceManager.GetString("ChangesRequireRestart", resourceCulture);
+            }
+        }
+        
+	public static string SupportedOnWindowsOnly {
+            get {
+                return ResourceManager.GetString("SupportedOnWindowsOnly", resourceCulture);
             }
         }
     }
